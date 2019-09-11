59276f1d7e97c2e0ee8ae666eee924a236991b13
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-11 06:23:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,7 @@
 first_number = 3
 second_number = 4 
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number*
+sum = first_number+second_number
+difference = first_number-second_number
+product = first_number*second_number
+quotient = first_number/second_number
+
